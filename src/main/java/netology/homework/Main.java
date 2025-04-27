ffb59c0e193db4f5de162697a1f068508dd964e1
Maxim U <sermax@bk.ru>
2025-04-28 01:10:42 +0400

    Initial commit

---
+++ src/main/java/netology/homework/Main.java
@@ -0,0 +1,17 @@
+package netology.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(1000, true);
+        System.out.println(actual);
+
+    }
+}
